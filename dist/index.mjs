017f1e7e520a19c6f1e9862833d17e87be80658a
Cosmin Popovici <hellocosmin@gmail.com>
2024-07-12 18:52:20 +0300

    feat: hybrid esm/cjs

---
+++ dist/index.mjs
@@ -0,0 +1,28 @@
+import omit from 'lodash.omit';
+import matchHelper from 'posthtml-match-helper';
+
+const plugin = (options = {}) => (tree) => {
+  options.attributes = options.attributes || {};
+  options.overwrite = options.overwrite || false;
+  const process = (node) => {
+    const attributes = Object.keys(options.attributes);
+    const matchers = attributes.map((attribute) => matchHelper(attribute));
+    attributes.forEach((key, i) => {
+      tree.match(matchers[i], (node2) => {
+        for (const [k, v] of Object.entries(options.attributes[key])) {
+          if (k === "class" && node2.attrs && node2.attrs.class) {
+            node2.attrs.class = [.../* @__PURE__ */ new Set([...node2.attrs.class.split(" "), ...v.split(" ")])].join(" ");
+          } else {
+            const attributes2 = options.overwrite ? options.attributes[key] : omit(options.attributes[key], Object.keys(node2.attrs || {}));
+            node2.attrs = { ...node2.attrs, ...attributes2 };
+          }
+        }
+        return node2;
+      });
+    });
+    return node;
+  };
+  return tree.walk(process);
+};
+
+export { plugin as default };
